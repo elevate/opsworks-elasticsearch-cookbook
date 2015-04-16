7db709d26346170904c6f0b9d9146b7a987faeaa
Mauricio Scheffer <mauricioscheffer@gmail.com>
2015-04-16 11:56:19 +0100

    Try access/secret keys

--- layer-custom/recipes/elevate-plugin-download.rb
+++ layer-custom/recipes/elevate-plugin-download.rb
@@ -5,6 +5,8 @@ end
 chef_gem "aws-sdk-v1"
 
 version = node.elasticsearch['elevate']['version']
+accessKey = node.elasticsearch['elevate']['plugin_access_key']
+secretKey = node.elasticsearch['elevate']['plugin_secret_key']
 fileName = "elasticsearch-elevate-plugin-#{version}.zip"
 localPath = "/home/ec2-user/#{fileName}"
 remotePath = "/release/com/elevatedirect/elasticsearch/plugin/elasticsearch-elevate-plugin/#{version}/#{fileName}"
@@ -14,9 +16,9 @@ ruby_block "download-object" do
   block do
       require 'aws-sdk-v1'
 
-      # http://docs.aws.amazon.com/AWSSdkDocsRuby/latest/DeveloperGuide/ruby-dg-roles.html
-      AWS.config(:credential_provider => AWS::Core::CredentialProviders::EC2Provider.new)
-
+	  # http://docs.aws.amazon.com/AWSRubySDK/latest/AWS/S3.html
+      AWS.config(:access_key_id => accessKey, :secret_access_key => secretKey)
+      
       s3 = AWS::S3.new
       s3obj = s3.buckets['elevate-es-plugins'].objects[remotePath]
       File.open(localPath, 'wb') do |file|
