3530c275245ae033b7570e75d748b0ed5b532144
Mauricio Scheffer <mauricioscheffer@gmail.com>
2015-04-17 11:15:44 +0100

    Fix remote path. Also no explicit key is needed now, IAM works

--- layer-custom/recipes/elevate-plugin-download.rb
+++ layer-custom/recipes/elevate-plugin-download.rb
@@ -5,19 +5,14 @@ end
 chef_gem "aws-sdk-v1"
 
 version = node.elasticsearch['elevate']['version']
-accessKey = node.elasticsearch['elevate']['plugin_access_key']
-secretKey = node.elasticsearch['elevate']['plugin_secret_key']
 fileName = "elasticsearch-elevate-plugin-#{version}.zip"
 localPath = "/home/ec2-user/#{fileName}"
-remotePath = "/release/com/elevatedirect/elasticsearch/plugin/elasticsearch-elevate-plugin/#{version}/#{fileName}"
+remotePath = "release/com/elevatedirect/elasticsearch/plugin/elasticsearch-elevate-plugin/#{version}/#{fileName}"
 
 ruby_block "download-object" do
 
   block do
       require 'aws-sdk-v1'
-
-	  # http://docs.aws.amazon.com/AWSRubySDK/latest/AWS/S3.html
-      AWS.config(:access_key_id => accessKey, :secret_access_key => secretKey)
       
       s3 = AWS::S3.new
       s3obj = s3.buckets['elevate-es-plugins'].objects[remotePath]
