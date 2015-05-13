52c176918a33193b04c79f4298237f3473b1e1d6
Mauricio Scheffer <mauricioscheffer@gmail.com>
2015-05-13 17:33:11 +0100

    Fix plugin local path

--- layer-custom/recipes/elevate-plugin-install.rb
+++ layer-custom/recipes/elevate-plugin-install.rb
@@ -1,12 +1,16 @@
 Chef::Log.info("******Installing Elevate Elasticsearch Plugin.******")
 
+version = node.elasticsearch['elevate']['version']
+fileName = "elasticsearch-elevate-plugin-#{version}.zip"
+localPath = "/home/ec2-user/#{fileName}"
+
 script "elevate_plugin_install" do
 	interpreter "bash"
 	user "root"
 	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
 	code <<-EOH
 	plugin -r elevate
-  	plugin -i elevate -u file:///home/ec2user/#{node.elasticsearch['elevate']['plugin-file']}
+  	plugin -i elevate -u file://#{localPath}
   	EOH
 	
 	notifies :restart, 'service[elasticsearch]'
